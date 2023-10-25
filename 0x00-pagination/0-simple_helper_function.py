3bab5246acbf572c959f809bc85af9adcb0efdfb
Luis Saiete <luistsaiete@gmail.com>
2023-10-25 20:10:19 +0200

    Fix pycodestyle for task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,6 +7,7 @@ one function called index_range
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for those
